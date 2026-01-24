042eb570e754c2c8a590807f69758c5c8071536a
Gaurav Jawalkar <gauravjawalkar8@gmail.com>
2026-01-24 18:51:09 +0530

    Add login link to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
       <h1>Hi School ERP frontend</h1>
+      <Link href={'/login'}>Login</Link>
     </div>
   );
 }
