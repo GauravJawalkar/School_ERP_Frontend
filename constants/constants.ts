3fd82eed5e1f021474ec8ed8210b61b70ccb2b98
Gaurav Jawalkar <gauravjawalkar8@gmail.com>
2026-01-28 23:18:41 +0530

    shifting from render to localhost :8000 for backend apiURLs for faster response in development

--- constants/constants.ts
+++ constants/constants.ts
@@ -1,1 +1,2 @@
-export const BASE_URL = process.env.BASE_URL || "https://school-erp-backend-r7jw.onrender.com/api/v1";
+// export const BASE_URL = process.env.BASE_URL || "https://school-erp-backend-r7jw.onrender.com/api/v1";
+export const BASE_URL = process.env.BASE_URL || "http://localhost:8000/api/v1";
