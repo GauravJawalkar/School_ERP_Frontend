e78e1538595b28e91aba8bad40d6a932d77bbdbf
Gaurav Jawalkar <gauravjawalkar8@gmail.com>
2026-03-29 23:57:07 +0530

    Building the AddAdmin Modal for the SchoolAdmin Screen

---
+++ components/Dashboard/SuperAdmin/Modals/AddAdminModal.tsx
@@ -0,0 +1,12 @@
+
+const AddAdminModal = ({ isOpen }: { isOpen: boolean }) => {
+
+    if (!isOpen) return null;
+    return (
+        <div>
+            AddAdminModal
+        </div>
+    )
+}
+
+export default AddAdminModal
